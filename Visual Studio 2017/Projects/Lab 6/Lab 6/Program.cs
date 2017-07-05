f60e36e4216f2084e4eb61e5f5e3bd49a1a6abc1
Tallon Simmons <tsims340@gmail.com>
2017-07-05 00:28:06 -0400

    first commit

---
+++ Visual Studio 2017/Projects/Lab 6/Lab 6/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    public class PigLatinClass
+    {
+        static void Main(string[] args)
+        { }
+
+           
+        
+
+            public static void pigTalk(string sentence)
+            {
+                try
+                {
+                    while (sentence != "exit")
+                    {
+                        string firstLetter;
+                        string afterFirst;
+                        string pigLatinOut = "";
+                        int x;
+                        string vowel = "AEIOUaeiou";
+
+                        Console.WriteLine("Enter a sentence to convert into PigLatin");
+
+                        sentence = Console.ReadLine();
+
+                        string[] pieces = sentence.Split();
+
+                        foreach (string piece in pieces)
+                        {
+                            afterFirst = piece.Substring(1);
+                            firstLetter = piece.Substring(0, 1);
+                            x = vowel.IndexOf(firstLetter);
+
+                            if (x == -1)
+                            {
+                                pigLatinOut = (afterFirst + firstLetter + "ay ");
+                            }
+                            else
+                            {
+                                pigLatinOut = (firstLetter + afterFirst + "way ");
+                            }
+
+                            Console.Write(pigLatinOut);
+                        }
+
+                        Console.WriteLine("Press Enter to flip the sentence back.");
+                        Console.ReadKey(true);
+                        string clonedString = null;
+                        clonedString = (String)sentence.Clone();
+                        Console.WriteLine(clonedString);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+            }
+        }
+    }
+
+    
+
